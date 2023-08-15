ee3e738ea0b5822a3d4b3e5864366fa442aeee09
Lidio Gabriel <lidiogabriel4@gmail.com>
2023-08-15 15:09:45 -0300

    ok

---
+++ src/pages/netflix/index.js
@@ -0,0 +1,19 @@
+import './index.scss';
+import { useState } from 'react';
+import { Axios } from 'axios';
+
+
+export default function Netflix(){
+    
+    
+    return(
+        <div className='pagina-netflix'>
+            <div className='container-friends'>
+                <nav>
+                    
+                </nav>
+            </div>
+        </div>
+    )
+
+}
